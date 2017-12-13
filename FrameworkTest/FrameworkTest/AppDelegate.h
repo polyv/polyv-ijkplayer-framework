1c1ea017c11de62bd37eb5e0487e4af7bac3d211
bqlin <bqlins@163.com>
2017-12-13 09:30:35 +0800

    添加测试 demo

---
+++ FrameworkTest/FrameworkTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameworkTest
+//
+//  Created by Bq Lin on 2017/12/8.
+//  Copyright © 2017年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrameworkTest/FrameworkTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameworkTest
+//
+//  Created by Bq Lin on 2017/12/8.
+//  Copyright © 2017年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
